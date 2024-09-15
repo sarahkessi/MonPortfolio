ef216064e212f0a45607945faebd80918dd26ae7
sarahk <2156390@cmontmorency.qc.ca>
2024-09-14 21:13:53 -0400

    Component Navbar

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,9 +1,10 @@
 import styles from "./App.module.css";
+import { NavBar } from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className={styles.App}>
-      <>Hello World !</>
+      <NavBar/>
     </div>
   );
 }
